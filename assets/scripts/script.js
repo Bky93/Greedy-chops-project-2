ab825df4cf53984cf99d47bb7a9cb3c0149cd02c
Rebeka Knight <rebeka_knight@hotmail.co.uk>
2024-06-07 13:45:58 +0000

    added js to reload the page when the user clicks on the reset button

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,28 @@
+const resetButton = document.getElementById("reset-button");
+
 //Modal
 var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
+var modalBtn = document.getElementById("how-to-play-btn");
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
+// When the user clicks the how to play button the how to play modal opens
+modalBtn.onclick = function () {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on the (x) in the modal it will close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal the modal will close
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+//Reset button
+resetButton.addEventListener("click", () => {
+    location.reload();
+});
